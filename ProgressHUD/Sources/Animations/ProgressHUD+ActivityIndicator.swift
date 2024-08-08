5b668ec7d39e096fef8cd0def29ad15c578ec80b
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-08-08 17:14:28 +0400

    test

--- ProgressHUD/Sources/Animations/ProgressHUD+ActivityIndicator.swift
+++ ProgressHUD/Sources/Animations/ProgressHUD+ActivityIndicator.swift
@@ -11,17 +11,47 @@
 
 import UIKit
 
+class ActivityIndicatorOldStyle: UIImageView {
+    
+    var hidesWhenStopped : Bool = true
+
+    override func startAnimating() {
+        isHidden = false
+        rotate()
+    }
+
+    override func stopAnimating() {
+        if hidesWhenStopped {
+            isHidden = true
+            removeRotation()
+        }
+    }
+
+    private func rotate() {
+        let rotation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 1
+        rotation.isCumulative = true
+        rotation.repeatCount = Float.greatestFiniteMagnitude
+        layer.add(rotation, forKey: "rotationAnimation")
+    }
+
+    private func removeRotation() {
+        layer.removeAnimation(forKey: "rotationAnimation")
+    }
+}
+
 // MARK: - Activity Indicator
 extension ProgressHUD {
 
-	func animationActivityIndicator(_ view: UIView) {
-		let spinner = UIActivityIndicatorView(style: .large)
-		let scale = view.frame.size.width / spinner.frame.size.width
-		spinner.transform = CGAffineTransform(scaleX: scale, y: scale)
-		spinner.frame = view.bounds
-		spinner.color = colorAnimation
-		spinner.hidesWhenStopped = true
-		spinner.startAnimating()
-		view.addSubview(spinner)
-	}
+    func animationActivityIndicator(_ view: UIView) {
+        
+        let spinner = ActivityIndicatorOldStyle(image: UIImage(named: "ic.loader"))
+        let scale = view.frame.size.width / spinner.frame.size.width
+        spinner.transform = CGAffineTransform(scaleX: scale, y: scale)
+        spinner.frame = view.bounds
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        view.addSubview(spinner)
+    }
 }
